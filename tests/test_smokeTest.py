44bc1a2336903481f1c41e8b55057809a869a8d3
Zamarron <emilio.zamarrons@gmail.com>
2024-12-18 15:50:29 -0600

    Adding file

--- tests/test_smokeTest.py
+++ tests/test_smokeTest.py
@@ -11,8 +11,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 
-class TestTestSmokesidepy():
-  
+class TestSmokeTest():
   def setup_method(self, method):
     options = Options()
     options.add_argument("--headless=new")
@@ -22,48 +21,69 @@ class TestTestSmokesidepy():
   def teardown_method(self, method):
     self.driver.quit()
   
-  def test_testSmokesidepy(self):
-    self.driver.get("http://127.0.0.1:5502/teton/1.6/index.html")
+  def test_homePagept1(self):
+    self.driver.get("https://zamarr0n.github.io/cse270-teton/index.html")
     self.driver.set_window_size(1440, 812)
-    self.driver.find_element(By.LINK_TEXT, "Home").click()
     elements = self.driver.find_elements(By.CSS_SELECTOR, ".header-logo img")
     assert len(elements) > 0
-    self.driver.find_element(By.CSS_SELECTOR, ".home-main").click()
     elements = self.driver.find_elements(By.CSS_SELECTOR, ".header-title > h1")
     assert len(elements) > 0
+    elements = self.driver.find_elements(By.CSS_SELECTOR, ".header-title > h2")
+    assert len(elements) > 0
+    self.driver.find_element(By.CSS_SELECTOR, "body").click()
     assert self.driver.title == "Teton Idaho CoC"
-    self.driver.find_element(By.LINK_TEXT, "Home").click()
-    self.driver.execute_script("window.scrollTo(0,1333)")
-    elements = self.driver.find_elements(By.CSS_SELECTOR, ".spotlight1 > .centered-image")
+  
+  def test_homePagept2(self):
+    self.driver.get("https://zamarr0n.github.io/cse270-teton/index.html")
+    self.driver.set_window_size(1440, 812)
+    elements = self.driver.find_elements(By.CSS_SELECTOR, ".spotlight1 > p:nth-child(3)")
     assert len(elements) > 0
     elements = self.driver.find_elements(By.CSS_SELECTOR, ".spotlight2 > .centered-image")
     assert len(elements) > 0
     elements = self.driver.find_elements(By.LINK_TEXT, "Join Us")
     assert len(elements) > 0
     self.driver.find_element(By.LINK_TEXT, "Join Us").click()
-    self.driver.find_element(By.LINK_TEXT, "Directory").click()
-    self.driver.find_element(By.ID, "directory-grid").click()
-    elements = self.driver.find_elements(By.CSS_SELECTOR, ".gold-member:nth-child(9) > img")
-    assert len(elements) > 0
-    self.driver.find_element(By.ID, "directory-list").click()
-    elements = self.driver.find_elements(By.CSS_SELECTOR, ".gold-member:nth-child(9) > p:nth-child(2)")
-    assert len(elements) > 0
+  
+  def test_joinpage(self):
+    self.driver.get("https://zamarr0n.github.io/cse270-teton/index.html")
+    self.driver.set_window_size(1440, 812)
     self.driver.find_element(By.LINK_TEXT, "Join").click()
+    elements = self.driver.find_elements(By.NAME, "fname")
+    assert len(elements) > 0
     self.driver.find_element(By.NAME, "fname").click()
     self.driver.find_element(By.NAME, "fname").send_keys("Jose Emilio")
     self.driver.find_element(By.NAME, "lname").send_keys("Zamarron Sanchez")
     self.driver.find_element(By.NAME, "bizname").click()
     self.driver.find_element(By.NAME, "bizname").send_keys("CS50")
-    self.driver.find_element(By.NAME, "biztitle").click()
+    self.driver.find_element(By.CSS_SELECTOR, ".myinput:nth-child(5)").click()
     self.driver.find_element(By.NAME, "biztitle").send_keys("Software developer ")
+    self.driver.find_element(By.CSS_SELECTOR, ".myinput:nth-child(2)").click()
+    self.driver.find_element(By.CSS_SELECTOR, ".myinput:nth-child(3)").click()
     self.driver.find_element(By.NAME, "submit").click()
     elements = self.driver.find_elements(By.NAME, "email")
     assert len(elements) > 0
+  
+  def test_directorypage(self):
+    self.driver.get("https://zamarr0n.github.io/cse270-teton/index.html")
+    self.driver.set_window_size(1440, 812)
+    self.driver.find_element(By.LINK_TEXT, "Directory").click()
+    elements = self.driver.find_elements(By.CSS_SELECTOR, ".gold-member:nth-child(9) > img")
+    assert len(elements) > 0
+    self.driver.find_element(By.ID, "directory-list").click()
+    elements = self.driver.find_elements(By.CSS_SELECTOR, ".gold-member:nth-child(9) > p:nth-child(2)")
+    assert len(elements) > 0
+    self.driver.find_element(By.ID, "directory-grid").click()
+  
+  def test_adminpage(self):
+    self.driver.get("https://zamarr0n.github.io/cse270-teton/index.html")
+    self.driver.set_window_size(1440, 812)
     self.driver.find_element(By.LINK_TEXT, "Admin").click()
+    elements = self.driver.find_elements(By.ID, "username")
+    assert len(elements) > 0
     self.driver.find_element(By.ID, "username").click()
     self.driver.find_element(By.ID, "username").send_keys("Emilio_Zamarron")
     self.driver.find_element(By.ID, "password").click()
-    self.driver.find_element(By.ID, "password").send_keys("scnijnejkjcsnckjsc")
+    self.driver.find_element(By.ID, "password").send_keys("csgrdfefw424424")
     self.driver.find_element(By.CSS_SELECTOR, ".mysubmit:nth-child(4)").click()
     WebDriverWait(self.driver, 30).until(expected_conditions.text_to_be_present_in_element((By.CSS_SELECTOR, ".errorMessage"), "Invalid username and password."))
   
